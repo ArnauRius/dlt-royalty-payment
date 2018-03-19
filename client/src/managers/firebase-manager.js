f2b9b224991b1b5fed921df6c00fd532ac2877d4
Sarquella <adrisarquella@gmail.com>
2018-03-19 18:16:46 +0100

    [N] Firebase added

---
+++ client/src/managers/firebase-manager.js
@@ -0,0 +1,12 @@
+/* Handles the configuration and manages everything related to Firebase */
+
+var config = {
+  apiKey: "AIzaSyDf19kF5Mmuip0ffS0HzUpK6QzdFpYUel0",
+  authDomain: "dlt-royalty-payment.firebaseapp.com",
+  databaseURL: "https://dlt-royalty-payment.firebaseio.com",
+  projectId: "dlt-royalty-payment",
+  storageBucket: "dlt-royalty-payment.appspot.com",
+  messagingSenderId: "624288298174"
+}
+
+firebase.initializeApp(config)
