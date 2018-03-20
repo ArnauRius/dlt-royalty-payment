7687fffb6739ef530775bebea2e2271a5425e6ce
Arnau Rius <arnau.rius.contact@gmail.com>
2018-03-20 13:19:27 +0100

    [R] Name account route, refactor checkAuth function, redirect to home from base url, structure parent-children structure urls

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,24 +7,30 @@ import Account from './components/pages/account.vue'
 
 // Defines the routes
 export default [
-  {path: '/', beforeEnter: redirectHome},
-  {path: '/home', component: Home, name: 'home'},
-  {path: '/account', component: Account, beforeEnter: checkAuth}
+  {
+    path: '/', beforeEnter: redirectHome, children: [
+    {path: 'home', component: Home, name: 'home'},
+    {path: 'account', component: Account, name: 'account', beforeEnter: checkAuth}
+  ]
+  }
 ]
 
-// Checks if user is authenticated.If not, redirect always to the '/home' route
+
+/**
+ * Checks if user is authenticated. If not, redirect always to the '/home' route
+ */
 function checkAuth(to, from, next) {
-  if (to !== '/home') {
-    if (!store.getters['user/isUserLogged']) {
-      redirectHome(to, from, next)
-    } else {
-      next()
-    }
+  if (!store.getters['user/isUserLogged']) {
+    redirectHome(to, from, next)
+  } else {
+    next()
   }
-  next()
 }
 
-// Redirects to the '/home' route
+
+/**
+ * Redirects to the '/home' route
+ */
 function redirectHome(to, from, next) {
   next({name: 'home'})
 }
