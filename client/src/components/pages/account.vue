a4436ea6f6c4edee502c6ca03e4e6afa8a00174f
Sarquella <adrisarquella@gmail.com>
2018-03-20 11:32:49 +0100

    [I] Name & Email are now on account page

--- client/src/components/pages/account.vue
+++ client/src/components/pages/account.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>THIS IS THE ACCOUNT PAGE</h1>
+    <h5>Name:</h5>
+    <p>{{ user.name }}</p>
+    <h5>Email: </h5>
+    <p>{{ user.email }}</p>
     <h5>Public key:</h5>
     <p>{{ user.signer.getPublicKey().asHex() }}</p>
   </div>
