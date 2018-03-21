574b8c358a96cef13f10106ba44b1d79bd2131d7
Arnau Rius <arnau.rius.contact@gmail.com>
2018-03-21 11:54:35 +0100

    [I] Account Page UI

--- client/src/components/pages/account.vue
+++ client/src/components/pages/account.vue
@@ -1,36 +1,38 @@
 <template>
   <div>
     <div class="card">
-      <div class="card-body mt-1">
-        <h1>ACCOUNT</h1>
-        <h5>Name:</h5>
-        <p>{{ user.name }}</p>
-        <h5>Email: </h5>
-        <p>{{ user.email }}</p>
-        <h5>Public key:</h5>
-        <p>{{ user.signer.getPublicKey().asHex() }}</p>
-        <h5>Artist:</h5>
-        <p>{{ user.isArtist }}</p>
+        <div class="card-header">
+            <h4>Account</h4>
+        </div>
+        <div class="card-body mt-1">
+          <h5>Name:</h5>
+          <p>{{ user.name }}</p>
+          <h5>Email: </h5>
+          <p>{{ user.email }}</p>
+          <h5>Public key:</h5>
+          <p>{{ user.signer.getPublicKey().asHex() }}</p>
+          <h5>Artist:</h5>
+          <p>{{ user.isArtist }}</p>
+        <!-- Menu right button -->
+        <button
+          v-if="user.isArtist"
+          class="btn btn-primary mt-5"
+          type="button"
+          data-toggle="modal"
+          data-target="#becomeArtistModal">
+          Go to Dashboard
+        </button>
+        <button
+          v-else
+          class="btn btn-primary mt-5"
+          type="button"
+          data-toggle="modal"
+          data-target="#becomeArtistModal">
+          Become an Artist
+        </button>
       </div>
+      <become-artist-modal id="becomeArtistModal"></become-artist-modal>
     </div>
-    <!-- Menu right button -->
-    <button
-            v-if="user.isArtist"
-            class="btn btn-primary mt-5"
-            type="button"
-            data-toggle="modal"
-            data-target="#becomeArtistModal">
-      Go to Dashboard
-      </button>
-    <button
-      class="btn btn-primary mt-5"
-      type="button"
-      data-toggle="modal"
-      data-target="#becomeArtistModal"
-      v-else>
-      Become an Artist
-    </button>
-    <become-artist-modal id="becomeArtistModal"></become-artist-modal>
   </div>
 </template>
 
