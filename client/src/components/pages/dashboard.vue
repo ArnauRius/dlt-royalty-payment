1452fd47c8b2498ba932c1b0c2970c857de71fde
Sarquella <adrisarquella@gmail.com>
2018-03-21 14:58:57 +0100

    [B] Account page checks for user auth again

--- client/src/components/pages/dashboard.vue
+++ client/src/components/pages/dashboard.vue
@@ -3,13 +3,13 @@
     <div class="col-sm-6"><h4>Dashboard</h4></div>
     <div class="col-sm-6 text-right"><div class="btn btn-primary ">Upload Song</div></div>
   </div>
-  <div class="row" v-for="song in songs">
+  <!--<div class="row" v-for="song in songs">
       <div class="card col-sm-6 col-md-3">
         <h5 class="card-title">{{ song.name }}</h5>
         <div class="btn btn-primary">Edit</div>
 
       </div>
-  </div>
+  </div>-->
 </template>
 
 <script>
