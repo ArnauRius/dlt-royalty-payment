25ea1277236d2168d4e73cd4442e97e0560549a0
Arnau Rius <arnau.rius.contact@gmail.com>
2018-06-02 12:56:43 +0200

    [I] Add post call for add operations

--- server-offchain/src/routes.js
+++ server-offchain/src/routes.js
@@ -7,6 +7,8 @@ module.exports = function(app){
     app.get('/api/artist/:id/', api.artist) // get one artist by id
     app.get('/api/user/:id/', api.user) // get one user by id
     app.get('/api/song/:id/', api.song) // get one song by id
-    app.post('/api/add/user/', api.addUser)
+    app.post('/api/artist/', api.addArtist) // add artist
+    app.post('/api/user/', api.addUser) // add user
+    app.post('/api/song/', api.addSong) // add song
 }
 
